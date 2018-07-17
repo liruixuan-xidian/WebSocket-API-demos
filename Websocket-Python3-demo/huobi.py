56e99098d969060a5ba670c9eb3fd1be426c6c7e
ruixuan.li <liruixuan.xidian@gmail.com>
2018-07-17 22:21:05 +0800

    insert mongodb
    Signed-off-by: ruixuan.li <liruixuan.xidian@gmail.com>

--- Websocket-Python3-demo/huobi.py
+++ Websocket-Python3-demo/huobi.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 #author: 半熟的韭菜
-
 from websocket import create_connection
 import gzip
 import time
@@ -8,33 +7,35 @@ import time
 if __name__ == '__main__':
     while(1):
         try:
-            ws = create_connection("wss://api.huobipro.com/ws")
+            #ws = create_connection("wss://api.huobipro.com/ws")
+            ws = create_connection("wss://api.hadax.com/ws")
             break
         except:
             print('connect ws error,retry...')
             time.sleep(5)
 
     # 订阅 KLine 数据
-    tradeStr="""{"sub": "market.ethusdt.kline.1min","id": "id10"}"""
+    tradeStr="""{"sub": "market.seelebtc.kline.1min","id": "id10"}"""
 
     # 请求 KLine 数据
-    # tradeStr="""{"req": "market.ethusdt.kline.1min","id": "id10", "from": 1513391453, "to": 1513392453}"""
+    #tradeStr="""{"req": "market..kline.1min","id": "id10", "from": 1513391453, "to": 1513392453}"""
 
     #订阅 Market Depth 数据
-    # tradeStr="""{"sub": "market.ethusdt.depth.step5", "id": "id10"}"""
+    tradeStr="""{"sub": "market.seelebtc.depth.step5", "id": "id10"}"""
 
     #请求 Market Depth 数据
     # tradeStr="""{"req": "market.ethusdt.depth.step5", "id": "id10"}"""
 
     #订阅 Trade Detail 数据
-    # tradeStr="""{"sub": "market.ethusdt.trade.detail", "id": "id10"}"""
+    tradeStr="""{"sub": "market.seelebtc.trade.detail", "id": "id10"}"""
 
     #请求 Trade Detail 数据
     # tradeStr="""{"req": "market.ethusdt.trade.detail", "id": "id10"}"""
 
     #请求 Market Detail 数据
     # tradeStr="""{"req": "market.ethusdt.detail", "id": "id12"}"""
-
+    client = MongoClient('mongodb://ds137611.mlab.com:37611/huobi')
+    db = client.example
     ws.send(tradeStr)
     while(1):
         compressData=ws.recv()
@@ -46,5 +47,6 @@ if __name__ == '__main__':
             ws.send(tradeStr)
         else:
             print(result)
+            db.auto.insert(result)
 
     
